f81b6e54606aaff3b3a0e2c4adc232e54943fe30
Maya Kadka <mayakakda@gmail.com>
2013-12-20 18:49:09 +0530

    Twitter and linkedIn follow buttons script

--- application/modules/socialNetwork/controllers/LinkedinController.php
+++ application/modules/socialNetwork/controllers/LinkedinController.php
@@ -6,7 +6,6 @@ class SocialNetwork_LinkedinController extends Zend_Controller_Action {
      * @url http://web-tools.local/social/linkedin_follow
      */
     public function followAction() {
-        echo '<br />follow action - linkedin';
     }
 
 }
--- application/modules/socialNetwork/controllers/TwitterController.php
+++ application/modules/socialNetwork/controllers/TwitterController.php
@@ -6,7 +6,7 @@ class SocialNetwork_TwitterController extends Zend_Controller_Action {
      * @url http://web-tools.local/social/twitter_follow
      */
     public function followAction() {
-        echo '<br />follow action - twitter';
+        
     }
 
 }
